a4674744a0ad563579b8822c94d38559fa78e34e
natsumikobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2023-03-24 07:20:52 +0000

    [Finish]java-13 swiper

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,4 @@
+/* global $*/
 let title = "javascriptが使えました"
 alert(title);
 
@@ -5,4 +6,21 @@ $(document).ready(function () {
   $('.jquery').on('click', function(){
     $(this).css('color','red');
   });
+});
+
+//Swiperのオプションを定数化
+const opt = {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+}
+
+//Swiperを実行(初期化)
+$(document).on('turbolinks:load', function() {
+  let swiper = new Swiper('.swiper',opt);
 });
